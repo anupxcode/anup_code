510acc9b38ae3fe7d2be5863f397203ddc0bd151
Shamim Hossain <flaxsoft@gmail.com>
2025-02-09 22:40:56 +0600

    add name

--- dictonary.py
+++ dictonary.py
@@ -80,6 +80,7 @@
 # print(k)
 # Use the get method to print the value of the "model" key of the car dictionary.
 car =	{
+  "name": "Hello World",
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
